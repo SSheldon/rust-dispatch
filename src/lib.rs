06370e45bbef738daf7bb2c0838b86925ec455a9
Steven Sheldon <steven@sasheldon.com>
2015-04-19 20:06:13 -0700

    Added some crate documentation and examples.

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,47 @@
+/*!
+Rust wrapper for Apple's Grand Central Dispatch (GCD).
+
+GCD is an implementation of task parallelism that allows tasks tasks to be
+submitted to queues where they are scheduled to execute.
+
+For more information, see Apple's [Grand Central Dispatch reference](
+https://developer.apple.com/library/mac/documentation/Performance/Reference/GCD_libdispatch_Ref/index.html).
+
+# Serial Queues
+
+Serial queues execute tasks serially in FIFO order. The application's main
+queue is serial and can be accessed through the `Queue::main` function.
+
+```
+use dispatch::{Queue, QueueAttribute};
+
+let queue = Queue::create("com.example.rust", QueueAttribute::Serial);
+queue.async(|| println!("Hello"));
+queue.async(|| println!("World"));
+```
+
+# Concurrent Queues
+
+Concurrent dispatch queues execute tasks concurrently. GCD provides global
+concurrent queues that can be accessed through the `Queue::global` function.
+
+`Queue` has two methods that can simplify processing data in parallel, `apply`
+and `map`:
+
+```
+use dispatch::{Queue, QueuePriority};
+
+let queue = Queue::global(QueuePriority::Default);
+
+let mut nums = vec![1, 2];
+queue.apply(&mut nums, |x| *x += 1);
+assert!(nums == [2, 3]);
+
+let nums = queue.map(nums, |x| x.to_string());
+assert!(nums[0] == "2");
+```
+*/
+
 extern crate libc;
 
 use std::ffi::{CStr, CString};
